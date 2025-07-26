5f56e387164b87a48930ad70692326cb188052de
Saeed Badran <salouri@gmail.com>
2025-07-26 04:01:09 +0300

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict'
 
 exports.worker = require('./lib/worker')
-exports.rpc = require('./lib/rpc')
-exports.http = require('./lib/http')
-exports.utils = require('./lib/utils')
+exports.client = require('./lib/client')
+exports.server = require('./lib/server')
